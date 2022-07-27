373a58a8b5660e5a253f054a10ce226c276c8144
alpsden <mudits1999@gmail.com>
2022-07-27 17:55:38 +0100

    Operational functionality added
    Driver configured

--- include/pca9555.h
+++ include/pca9555.h
@@ -1,4 +1,9 @@
 
+/**
+ * @brief pca9555 driver for rp2040
+ *
+ */
+
 #ifndef PCA9555_H_
 #define PCA9555_H_
 
@@ -12,51 +17,6 @@
 #define LOW 0
 #define INPUT 1
 #define OUTPUT 0
-
-/** enum with names of ports */
-typedef enum
-{
-    pin0,
-    pin1,
-    pin2,
-    pin3,
-    pin4,
-    pin5,
-    pin6,
-    pin7,
-    pin8,
-    pin9,
-    pin10,
-    pin11,
-    pin12,
-    pin13,
-    pin14,
-    pin15
-} pin_num_e;
-
-enum
-{
-    io00,
-    io01,
-    io02,
-    io03,
-    io04,
-    io05,
-    io06,
-    io07
-} io_port_0_e;
-
-enum
-{
-    io10,
-    io11,
-    io12,
-    io13,
-    io14,
-    io15,
-    io16,
-    io17
-} io_port_1_e;
 
 enum
 {
@@ -75,17 +35,52 @@ typedef enum
     output,
     input,
     ALL_OUTPUT = 0,
-    ALL_INPUT = 0B11111111,
+    ALL_INPUT = 0B11111111
 } config_reg_val_e;
 
 typedef enum
 {
     retain,
     inverted,
-    ALL_RETAINED = 0,
+    ALL_RETAINED = 0B0,
     ALL_INVERTED = 0B11111111
 } polarity_reg_val_e;
 
+typedef enum
+{
+    OUT_LOW,
+    OUT_HIGH,
+    ALL_LOW = 0B0,
+    ALL_HIGH = 0B11111111
+} output_port_val_e;
+
+//******************************************************
+typedef union
+{
+    uint8_t all;
+    struct
+    {
+        uint8_t pin0 : 1;
+        uint8_t pin1 : 1;
+        uint8_t pin2 : 1;
+        uint8_t pin3 : 1;
+        uint8_t pin4 : 1;
+        uint8_t pin5 : 1;
+        uint8_t pin6 : 1;
+        uint8_t pin7 : 1;
+
+    } bits;
+
+} output_port_t;
+
+typedef struct
+{
+    output_port_t config_port0;
+    output_port_t config_port1;
+} output_ports_t;
+//*******************************************************
+
+//******************************************************
 typedef union
 {
     uint8_t all;
@@ -109,7 +104,9 @@ typedef struct
     config_port_t config_port0;
     config_port_t config_port1;
 } config_ports_t;
+//*******************************************************
 
+//*******************************************************
 typedef union
 {
     uint8_t all;
@@ -133,6 +130,7 @@ typedef struct
     polarity_port_t polarity_port0;
     polarity_port_t polarity_port1;
 } polarity_ports_t;
+//*********************************************************
 
 typedef union
 {
@@ -143,16 +141,75 @@ typedef union
 class pca9555
 {
 private:
+    /**
+     * @brief Device address
+     *
+     */
     uint8_t address;
+
+    /**
+     * @brief Gpio pin on which interrput occurs
+     *
+     */
     uint8_t int_pin_num;
-    uint16_t I2CGetValue(uint8_t address, uint8_t reg);
-    void I2CSetValue(uint8_t address, uint8_t reg, uint8_t value);
+
+    /**
+     * @brief I2C read function
+     *
+     * @param address
+     * @param reg
+     * @return uint16_t: read val from the regs
+     */
+    uint16_t twi_read(uint8_t address, uint8_t reg);
+
+    /**
+     * @brief i2c write function
+     *
+     * @param address
+     * @param reg
+     * @param value
+     * @param len
+     */
+    void twi_write(uint8_t address, uint8_t reg, uint8_t *value, uint8_t len);
 
 public:
     uint8_t _error;
+
+    /**
+     * @brief Construct a new pca9555 object
+     *
+     * @param address: Dev address of the io expander
+     * @param int_pin_num: pin number of the interrupt gpio (on the mcu)
+     * Set to zero if polling
+     */
     pca9555(uint8_t address, uint8_t int_pin_num);
+
+    /**
+     * @brief set pins as input or output
+     *
+     * @param config_ports: Struct defining the pin cofig (for each bank) as input and output
+     */
     void pin_mode(config_ports_t *config_ports);
+
+    /**
+     * @brief Set the output object
+     *
+     * @param output_ports Struct to set output port values
+     */
+    void set_output(output_ports_t *output_ports);
+
+    /**
+     * @brief Set the polarity object
+     *
+     * @param pol_ports: Struct defining the polarity of pins
+     */
     void set_polarity(polarity_ports_t *pol_ports);
+
+    /**
+     * @brief read values of the input regs
+     *
+     * @return uint16_t:
+     */
     uint16_t read_input();
 };
 
--- pca9555.cpp
+++ pca9555.cpp
@@ -4,6 +4,12 @@
 #include "pico/stdio.h"
 #include "pca9555.h"
 
+/**
+ * @brief Construct a new pca9555::pca9555 object
+ *
+ * @param address
+ * @param int_pin_num set to zero if polling
+ */
 pca9555::pca9555(uint8_t address, uint8_t int_pin_num)
 {
   this->address = address;
@@ -14,39 +20,74 @@ pca9555::pca9555(uint8_t address, uint8_t int_pin_num)
     gpio_init(int_pin_num);
     gpio_set_dir(int_pin_num, false);
     gpio_is_pulled_up(int_pin_num);
+
+    printf("hello from constructor %d\n", this->address);
   }
 
   else
   {
-    this->int_pin_num = 0;
+    int_pin_num = 0;
   }
 }
 
+/**
+ * @brief Set pins as input or output
+ *
+ * @param config_ports
+ */
 void pca9555::pin_mode(config_ports_t *config_ports)
 {
-  I2CSetValue(address, cp_0, config_ports->config_port0.all);
-  I2CSetValue(address, cp_1, config_ports->config_port1.all);
+  uint8_t reg_config[2] = {config_ports->config_port0.all, config_ports->config_port1.all};
+  twi_write(this->address, cp_0, reg_config, 2);
+  read_input();
 }
 
+/**
+ * @brief Set pins as input or output
+ *
+ * @param config_ports
+ */
+void pca9555::set_output(output_ports_t *output_ports)
+{
+  uint8_t reg_config[2] = {output_ports->config_port0.all, output_ports->config_port1.all};
+  twi_write(this->address, cp_0, reg_config, 2);
+  read_input();
+}
+
+/**
+ * @brief set polarity of the pins
+ *
+ * @param pol_ports
+ */
 void pca9555::set_polarity(polarity_ports_t *pol_ports)
 {
-  I2CSetValue(address, pi_0, pol_ports->polarity_port0.all);
-  I2CSetValue(address, pi_1, pol_ports->polarity_port1.all);
+  uint8_t pol_config[2] = {pol_ports->polarity_port0.all, pol_ports->polarity_port1.all};
+  twi_write(address, pi_0, pol_config, 2);
 }
 
+/**
+ * @brief read value of the input regs
+ *
+ * @return uint16_t
+ */
 uint16_t pca9555::read_input()
 {
-  return I2CGetValue(address, ip_0);
+  return twi_read(address, ip_0);
 }
 
-uint16_t pca9555::I2CGetValue(uint8_t address, uint8_t reg)
+/**
+ * @name twi_read(uint8_t address, uint8_t reg)
+ * @param address
+ * @param reg
+ * @return uint16_t
+ */
+uint16_t pca9555::twi_read(uint8_t address, uint8_t reg)
 {
   uint8_t _inputData[2];
   word_u words_;
-
-  _error = i2c_write_blocking(i2c_default, address, &reg, 1, false);
+  _error = i2c_write_blocking(i2c_default, address, &reg, 1, true);
   _error = i2c_read_blocking(i2c_default, address, (uint8_t *)&_inputData, 2, false);
-
+  // printf("%d", _error);
   words_.byte[0] = _inputData[0];
   words_.byte[1] = _inputData[1];
 
@@ -54,17 +95,20 @@ uint16_t pca9555::I2CGetValue(uint8_t address, uint8_t reg)
 }
 
 /**
- * @name I2CSetValue(uint8_t address, uint8_t reg, uint8_t value)
+ * @name twi_write(uint8_t address, uint8_t reg, uint8_t value, uint8_t len)
  * @param address Address of I2C chip
  * @param reg    register to write to
  * @param value    value to write to register
+ * @param len length of data being sent
  * Write the value given to the register set to selected chip.
  */
-void pca9555::I2CSetValue(uint8_t address, uint8_t reg, uint8_t value)
+void pca9555::twi_write(uint8_t address, uint8_t reg, uint8_t *value, uint8_t len)
 {
   //
   // write output register to chip
   //
-  _error = i2c_write_blocking(i2c_default, address, &reg, 1, false);   // setup direction registers
-  _error = i2c_write_blocking(i2c_default, address, &value, 1, false); // pointer to configuration register address 0
+  uint8_t err;
+  uint8_t _reg = reg;
+  err = i2c_write_blocking(i2c_default, address, &_reg, 1, false);   // setup direction registers
+  err = i2c_write_blocking(i2c_default, address, value, len, false); // pointer to configuration register address 0
 }
